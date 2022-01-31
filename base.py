fbc0af0489c4989cde3defada978da752beb6253
Renato Veneroso <rveneroso@gmail.com>
2022-01-31 16:52:06 -0300

    Inclusão de mais operações envolvendo conjuntos no arquivo base.py.

--- base.py
+++ base.py
@@ -31,18 +31,33 @@ usuarios_machine_learning = {13, 23, 56, 42}
 
 # Aplicar a operação or (|) sobre dois conjuntos devolve a união de ambos:
 usuarios_finais = usuarios_data_science | usuarios_machine_learning
-print(usuarios_finais)
+print(f'Resultado do operador | sobre os dois conjuntos {usuarios_finais}')
 
 # Já a intersecção de 2 conjuntos pode ser obtida através do operador and (&)
 usuarios_dois_cursos = usuarios_data_science & usuarios_machine_learning
-print(usuarios_dois_cursos)
+print(f'Resultado do operador & sobre os dois conjuntos {usuarios_dois_cursos}')
 
 # Se for necessário saber quais elementos estão presentes em um conjunto e não estão presentes no
 # outro deve-se usar o operador de menos (-)
 usuarios_que_nao_fizeram_machine_learning = usuarios_data_science - usuarios_machine_learning
-print(usuarios_que_nao_fizeram_machine_learning)
+print(f'Resultado do operador - sobre os dois conjuntos {usuarios_que_nao_fizeram_machine_learning}')
 
 # Para saber os usuários que só fizeram data science ou só fizeram machine learning, deve-se
 # utilizar o operador ^ (ou exclusivo)
 so_ds_ou_so_ml = usuarios_data_science ^ usuarios_machine_learning
 print(so_ds_ou_so_ml)
+
+# Pode-se adicionar elementos a um conjunto com o método add(). Caso o elemento a ser inserido já
+# exista no conjunto ele não será adicionado mas essa operação não levanta nenhum erro. Convém
+# lembrar que não é possível determinar a posição em que o novo elemento será inserido no conjunto.
+usuarios = {8, 32, 27, 54, 5, 2012}
+print(usuarios)
+usuarios.add(2012) # O elemento já existe e nada será feito.
+print(usuarios)
+usuarios.add(1967)
+print(usuarios)
+
+# Por padrão, sets são objetos mutáveis em Python. Caso seja necessário tornar um set imutável
+# deve-se utilizar o método frozenset:
+usuarios = frozenset(usuarios)
+# usuarios.add(666) # Resulta no erro AttributeError: 'frozenset' object has no attribute 'add'
